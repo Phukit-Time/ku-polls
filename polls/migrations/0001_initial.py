a9bec6f22093bca382aaa6fb4f74901668a42ecd
Phukit <phukit@ku.th>
2022-09-12 15:31:25 +0700

    add end date

--- polls/migrations/0001_initial.py
+++ polls/migrations/0001_initial.py
@@ -18,6 +18,7 @@ class Migration(migrations.Migration):
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('question_text', models.CharField(max_length=200)),
                 ('pub_date', models.DateTimeField(verbose_name='date published')),
+                ('end_date', models.DateTimeField(verbose_name='end vote date')),
             ],
         ),
         migrations.CreateModel(
