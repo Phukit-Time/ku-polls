eeb3b900dcffd42eea070c2890419934d67566db
Phukit <phukit@ku.th>
2022-09-12 18:03:38 +0700

    add is_published and can_vote and also some test cases for them but still get error

--- polls/migrations/0001_initial.py
+++ polls/migrations/0001_initial.py
@@ -18,7 +18,7 @@ class Migration(migrations.Migration):
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('question_text', models.CharField(max_length=200)),
                 ('pub_date', models.DateTimeField(verbose_name='date published')),
-                ('end_date', models.DateTimeField(verbose_name='end vote date')),
+                ('end_date', models.DateTimeField(verbose_name='null')),
             ],
         ),
         migrations.CreateModel(
--- polls/models.py
+++ polls/models.py
@@ -8,17 +8,21 @@ from django.utils import timezone
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
-    end_date = models.DateTimeField('end vote date')
+    end_date = models.DateTimeField('null')
 
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
     def is_published(self):
-        pass
+        now = timezone.now()
+        return now >= self.pub_date
 
     def can_vote(self):
-        pass
+        now = timezone.now()
+        if not self.end_date:
+            return self.pub_date <= now
+        return self.pub_date <= now <= self.end_date
 
     def __str__(self):
         return self.question_text
